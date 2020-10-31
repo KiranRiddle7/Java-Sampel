70e4daa5e62f8bfd9019ca43d70e17958b7ab7a7
KiranRiddle7 <Kiranriddle.kc29@gmail.com>
2020-10-31 18:13:58 -0400

    initial commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+	    int b = 20;
+	    int c;
+	    
+	    c = a+b;
+	    
+	    System.out.println("Value of C ="+c + "  "+ args[0]);
+	}
+
+}
